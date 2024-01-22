83b6d038ba4e3dbabd6da8ca182ebd987939b89b
rashedsahaji <asifrashed2@gmail.com>
2024-01-22 13:56:36 +0530

    feat: add download button

--- lib/app/modules/details_view/view/details_view.dart
+++ lib/app/modules/details_view/view/details_view.dart
@@ -1,6 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 
 import '../view_model/details_view_model.dart';
 
@@ -55,6 +59,34 @@ class _DetailViewState extends ConsumerState<DetailView> {
                     ),
                   ),
                   Text(data.value?.description ?? 'No Description'),
+                  Text("Likes: ${data.value?.likes ?? 0}"),
+                  TextButton(
+                      onPressed: () async {
+                        showDialog(
+                            context: context,
+                            builder: (context) => const Center(
+                                  child: CircularProgressIndicator(),
+                                ));
+                        var response = await Dio().get(
+                            data.value?.links?.download ?? '',
+                            options: Options(responseType: ResponseType.bytes));
+                        final result = await ImageGallerySaver.saveImage(
+                            Uint8List.fromList(response.data),
+                            quality: 60,
+                            name: data.value?.slug ?? '');
+                        Navigator.pop(context);
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return Dialog(
+                                  child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: const Text("Image Saved to Gallery"),
+                              ));
+                            });
+                        print(result);
+                      },
+                      child: Text("Download"))
                 ],
               ),
             );
